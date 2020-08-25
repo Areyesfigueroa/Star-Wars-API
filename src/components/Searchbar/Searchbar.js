5c1e1ce4f6a971369a825ca3f89c572f3898d433
Aliel Reyes <alielreyes@gmail.com>
2020-08-24 19:25:39 -0700

    Everything is now lowercase

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -11,6 +11,7 @@ const searchbar = (props) => {
             <InputGroup className="mb-3">
                 <FormControl
                 ref={props.reference}
+                className={classes.searchInput}
                 onKeyDown={(event) => props.keydown(event)}
                 placeholder="Search Character..."
                 aria-label="Searchbar"
