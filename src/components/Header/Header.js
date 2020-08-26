30d432313f3cfc9af1bef5bea90c36330dab8967
Aliel Reyes <alielreyes@gmail.com>
2020-08-25 19:20:29 -0700

    Rafactored Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Header.module.css';
+import Image from 'react-bootstrap/Image';
 
 import stormTrooperGif from '../../assets/images/stormtrooper.gif';
 import redSaber from '../../assets/images/Left_Red_01.png';
@@ -8,14 +9,14 @@ import blueSaber from '../../assets/images/Right_Blue_01.png';
 const header = () => {
     return (
         <header className={classes.Header}>
-            <img src={stormTrooperGif}/>
+            <Image src={stormTrooperGif}/>
             <div className={classes.title}>
                 <div>
-                    <img src={redSaber}/>
+                    <Image src={redSaber}/>
                 </div>
                 <h1>Star Wars APi</h1>
                 <div>
-                    <img src={blueSaber}/>
+                    <Image src={blueSaber}/>
                 </div>
             </div>
         </header>
